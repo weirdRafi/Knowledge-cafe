c6fab5372986992d192d0d3a7910c7fade5ae5ae
weirdRafi <mazharulrafi@gmail.com>
2023-03-31 01:30:48 +0600

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Showjob from './components/Showjob/Showjob';
 
 function App() {
   const [detail, setDetail] = useState([])
@@ -13,22 +14,23 @@ function App() {
     const newDetail = [...detail, j]
     // console.log(newDetail);
     setDetail(newDetail)
-    return (
-      <div className="App">
-        <Header></Header>
-        <div className='row'>
-          <div className='col-md-8'>
-            <Main handleAddToDetails={handleAddToDetails}></Main>
-          </div>
-          <div className='col-md-4'>
+  }
+  return (
+    <div className="App">
+      <Header></Header>
+      <div className='row'>
+        <div className='col-md-8'>
+          <Main></Main>
+        </div>
+        <div className='col-md-4'>
           {
-            detail.map(d => )
+            detail.map(d => <Showjob d={d}></Showjob>)
           }
-          </div>
         </div>
       </div>
+    </div>
 
-    )
-  }
+  )
+}
 
-  export default App
+export default App
---
+++ src/components/Showjob/Showjob.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Showjob = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Showjob;
