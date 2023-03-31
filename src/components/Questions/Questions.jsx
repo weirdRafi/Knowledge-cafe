9b3af883643378aea20b2b681f535c6b15dcd3b1
weirdRafi <mazharulrafi@gmail.com>
2023-03-31 15:33:51 +0600

    questions added

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -10,10 +10,10 @@ const Questions = () => {
             <p><b>Ans:</b> useState is React Hook that allows you to add state to a functional component. It returns an array with two values: the current state and a function to update it.</p>
             <br />
             <h3>Q: What else UseEffect can do except load data?</h3>
-            <p><b>Ans:</b></p>
+            <p><b>Ans:</b> You can use useEffect to update the UI in response to user input.</p>
             <br />
             <h3>Q: How does react works?</h3>
-            <p><b>Ans:</b> </p>
+            <p><b>Ans:</b> React is a JavaScript library that allows developers to build reusable and modular user interfaces (UI) using a component-based architecture. React works by creating a virtual representation of the UI in memory, and then rendering that virtual representation to the actual DOM. </p>
         </div>
     );
 };
